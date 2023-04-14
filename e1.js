e37719ea04bea59eaffcd68ab08d103d23ab1960
Jason Eustache <110081009+jasonEustache@users.noreply.github.com>
2023-04-14 14:15:32 -0400

    update

---
+++ e1.js
@@ -0,0 +1,79 @@
+// FUNCTIONS: Basics
+// BEFORE YOU START: please read the /exercises-info/e1.md file.
+
+/**
+ * ====================================================
+ * Create a function to work as follows:
+  * The function name is 'logMessage' strictly.
+  * The function logs a message in the console.
+  * The function does NOT require any arguments.
+ * Message to print: 'Hello, World!'
+ * logMessage() => 'Hello, World!'
+*/
+
+// Your code goes here
+function logMessage(){
+return console.log('Hello, World!');
+}
+
+
+/**
+ * =============================================================
+ * Create a function that works as follows:
+  * The function name is 'logMyMessage' strictly.
+  * The function logs a specific message in the console.
+  * The message is a string value passed as an argument
+ * Message example: 'I log my first message!'
+ * logMyMessage('My message') => 'My message'
+*/
+// Your code goes here...
+function logMyMessage(message){
+return console.log(message);
+}
+
+
+
+/**
+ * ===============================================================
+ * Create a  function that works as follows:
+  * The function name is 'logMessageWithMyName' strictly.
+  * The function logs a message of 'My name is <name>!'.
+  * The name is a string value passed as an argument
+ * logMessageWithMyName('Andrey') => 'My name is Andrey!'
+*/
+// Your code goes here...
+function logMessageWithMyName(name){
+return console.log(`My name is ${name}!`);
+}
+
+
+/**
+ * ==============================================================
+ * Create a function that works as follows:
+  * the function name is 'logMyInfo' strictly.
+  * The function of logs a message of 'My name is <name>, I am <age> years old!'.
+  * The name is a string value passed as an argument.
+  * The age is a number value passed as an argument.
+ * logMyInfo('Andrey', 18) => 'My name is Andrey. I am 18 years old!'
+*/
+// Your code goes here...
+function logMyInfo(name,age){
+return console.log(`My name is ${name}. I am ${age} years old!`);
+}
+
+
+// === TEST YOURSELF ===
+// Once you're finished run the test with "npm run test-1"
+// If the test has all tests passed, switch to the next exercise file
+// If any of the tests fails, refactor the code and run the test command after you've fixed the function
+
+
+// Do not delete or change the lines beneath
+const f = {
+  logMessage: logMessage || undefined,
+  logMyMessage: logMyMessage || undefined,
+  logMessageWithMyName: logMessageWithMyName || undefined,
+  logMyData: logMyInfo || undefined
+}
+
+export { f };
